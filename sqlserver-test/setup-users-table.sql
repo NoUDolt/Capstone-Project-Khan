c2558cb09434fcd767bbf07f849636bd02d9a843
Azzan <48100454+NoUDolt@users.noreply.github.com>
2026-02-19 20:25:08 -0500

    ....

--- sqlserver-test/setup-users-table.sql
+++ sqlserver-test/setup-users-table.sql
@@ -1,9 +1,4 @@
--- ==========================================
--- Plateful Users Table Setup
--- Run this script on your SQL Server database
--- ==========================================
 
--- Create Users table for auth
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Users')
 BEGIN
   CREATE TABLE dbo.Users (
@@ -20,8 +15,3 @@ ELSE
 BEGIN
   PRINT 'Users table already exists.';
 END
-
--- Optional: Create a default admin account (password: admin123)
--- The hash below is for 'admin123' using bcryptjs with 10 salt rounds
--- If you want to set your own password, register as a user first, then update the role in SQL:
--- UPDATE dbo.Users SET Role = 'admin' WHERE Username = 'your_username';
